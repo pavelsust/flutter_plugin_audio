75903ccdae49c98101fc255582c8fb162c783f4e
Masud Parvez <masud@Masuds-MacBook-Pro.local>
2026-04-24 01:05:20 +0600

    export added

--- android/src/main/kotlin/com/flutter_plugin_audio/FlutterPluginAudioPlugin.kt
+++ android/src/main/kotlin/com/flutter_plugin_audio/FlutterPluginAudioPlugin.kt
@@ -9,10 +9,8 @@ import android.content.ServiceConnection
 import android.os.Bundle
 import android.os.IBinder
 import android.util.Log
-import android.widget.Toast
 import androidx.annotation.NonNull
 import com.danielgauci.native_audio.isServiceRunning
-
 import io.flutter.embedding.engine.plugins.FlutterPlugin
 import io.flutter.embedding.engine.plugins.activity.ActivityAware
 import io.flutter.embedding.engine.plugins.activity.ActivityPluginBinding
@@ -20,18 +18,13 @@ import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugin.common.MethodChannel.Result
-import io.flutter.view.FlutterNativeView
 
-/** FlutterPluginAudioPlugin */
-class FlutterPluginAudioPlugin: FlutterPlugin, MethodCallHandler, ActivityAware {
+class FlutterPluginAudioPlugin : FlutterPlugin, MethodCallHandler, ActivityAware {
 
-
-  private lateinit var channel : MethodChannel
+  private lateinit var channel: MethodChannel
   private lateinit var context: Context
 
-
   companion object {
-    
     private const val NATIVE_METHOD_PLAY = "play"
     private const val NATIVE_METHOD_PLAY_ARG_URL = "url"
     private const val NATIVE_METHOD_PLAY_ARG_TITLE = "title"
@@ -51,6 +44,7 @@ class FlutterPluginAudioPlugin: FlutterPlugin, MethodCallHandler, ActivityAware 
     private const val NATIVE_METHOD_SET_SKIP_TIME = "setSkipTime"
     private const val NATIVE_METHOD_SET_SKIP_TIME_ARG_FORWARD_MILLIS = "forwardMillis"
     private const val NATIVE_METHOD_SET_SKIP_TIME_ARG_BACKWARD_MILLIS = "backwardMillis"
+
     private const val FLUTTER_METHOD_ON_LOADED = "onLoaded"
     private const val FLUTTER_METHOD_ON_LOADED_ARG_STARTED_AUTOMATICALLY = "startedAutomatically"
     private const val FLUTTER_METHOD_ON_LOADED_ARG_TOTAL_DURATION_IN_MILLIS = "totalDurationInMillis"
@@ -63,72 +57,98 @@ class FlutterPluginAudioPlugin: FlutterPlugin, MethodCallHandler, ActivityAware 
     private const val FLUTTER_METHOD_ON_PREVIOUS = "onPrevious"
 
     private var audioService: AudioService? = null
-
   }
 
   private var serviceConnection: ServiceConnection? = null
 
-  override fun onAttachedToEngine(@NonNull flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
-    channel = MethodChannel(flutterPluginBinding.binaryMessenger, "flutter_plugin_audio")
+  override fun onAttachedToEngine(@NonNull binding: FlutterPlugin.FlutterPluginBinding) {
+    context = binding.applicationContext
+    channel = MethodChannel(binding.binaryMessenger, "flutter_plugin_audio")
     channel.setMethodCallHandler(this)
-    context = flutterPluginBinding.applicationContext
-
   }
-  private var flutterView: FlutterNativeView? = null
+
   override fun onMethodCall(@NonNull call: MethodCall, @NonNull result: Result) {
-    if (call.method == "getPlatformVersion") {
-      result.success("Android ${android.os.Build.VERSION.RELEASE}")
-    } else {
-      result.notImplemented()
+    when (call.method) {
+      "getPlatformVersion" -> {
+        result.success("Android ${android.os.Build.VERSION.RELEASE}")
+        return
+      }
     }
 
-
     withService { service ->
-
       when (call.method) {
         NATIVE_METHOD_PLAY -> {
-          withArgument(call, NATIVE_METHOD_PLAY_ARG_URL) { url: String ->
-            // Get optional arguments
+          withArgument<String>(call, NATIVE_METHOD_PLAY_ARG_URL) { url ->
             val title = call.argument<String>(NATIVE_METHOD_PLAY_ARG_TITLE)
             val artist = call.argument<String>(NATIVE_METHOD_PLAY_ARG_ARTIST)
             val album = call.argument<String>(NATIVE_METHOD_PLAY_ARG_ALBUM)
             val imageUrl = call.argument<String>(NATIVE_METHOD_PLAY_ARG_IMAGE_URL)
-            val startAutomatically = call.argument<Boolean>(NATIVE_METHOD_PLAY_START_AUTOMATICALLY)
-            val startFromMillis = call.argument<Int>(NATIVE_METHOD_PLAY_START_FROM_MILLIS)
+            val startAutomatically =
+              call.argument<Boolean>(NATIVE_METHOD_PLAY_START_AUTOMATICALLY) ?: true
+            val startFromMillis =
+              call.argument<Int>(NATIVE_METHOD_PLAY_START_FROM_MILLIS)?.toLong() ?: 0L
 
-            // Call service
-            withService {
-              it.play(
-                      url = url,
-                      title = title,
-                      artist = artist,
-                      album = album,
-                      imageUrl = imageUrl,
-                      startAutomatically = startAutomatically ?: true,
-                      startFromMillis = startFromMillis?.toLong() ?: 0L
-              )
-            }
+            service.play(
+              url = url,
+              title = title,
+              artist = artist,
+              album = album,
+              imageUrl = imageUrl,
+              startAutomatically = startAutomatically,
+              startFromMillis = startFromMillis
+            )
+            result.success(null)
           }
         }
-        NATIVE_METHOD_RESUME -> service.resume()
-        NATIVE_METHOD_PAUSE -> service.pause()
-        NATIVE_METHOD_STOP -> service.stop()
-        NATIVE_METHOD_RELEASE -> releaseAudioService()
+
+        NATIVE_METHOD_RESUME -> {
+          service.resume()
+          result.success(null)
+        }
+
+        NATIVE_METHOD_PAUSE -> {
+          service.pause()
+          result.success(null)
+        }
+
+        NATIVE_METHOD_STOP -> {
+          service.stop()
+          result.success(null)
+        }
+
+        NATIVE_METHOD_RELEASE -> {
+          releaseAudioService()
+          result.success(null)
+        }
+
         NATIVE_METHOD_SEEK_TO -> {
-          withArgument(call, NATIVE_METHOD_SEEK_TO_ARG_TIME) { timeInMillis: Long ->
+          withArgument<Long>(call, NATIVE_METHOD_SEEK_TO_ARG_TIME) { timeInMillis ->
             service.seekTo(timeInMillis)
+            result.success(null)
           }
         }
-        NATIVE_METHOD_SKIP_FORWARD -> service.skipForward()
-        NATIVE_METHOD_SKIP_BACKWARD -> service.skipBackward()
+
+        NATIVE_METHOD_SKIP_FORWARD -> {
+          service.skipForward()
+          result.success(null)
+        }
+
+        NATIVE_METHOD_SKIP_BACKWARD -> {
+          service.skipBackward()
+          result.success(null)
+        }
+
         NATIVE_METHOD_SET_SKIP_TIME -> {
-          withArgument(call, NATIVE_METHOD_SET_SKIP_TIME_ARG_FORWARD_MILLIS) { forward: Long ->
-            withArgument(call, NATIVE_METHOD_SET_SKIP_TIME_ARG_BACKWARD_MILLIS) { rewind: Long ->
+          withArgument<Long>(call, NATIVE_METHOD_SET_SKIP_TIME_ARG_FORWARD_MILLIS) { forward ->
+            withArgument<Long>(call, NATIVE_METHOD_SET_SKIP_TIME_ARG_BACKWARD_MILLIS) { rewind ->
               AudioService.SKIP_FORWARD_TIME_MILLIS = forward
               AudioService.SKIP_BACKWARD_TIME_MILLIS = rewind
+              result.success(null)
             }
           }
         }
+
+        else -> result.notImplemented()
       }
     }
   }
@@ -137,63 +157,56 @@ class FlutterPluginAudioPlugin: FlutterPlugin, MethodCallHandler, ActivityAware 
     channel.setMethodCallHandler(null)
   }
 
-  /**
-   *   Flutter attached
-   *
-   */
-
   override fun onAttachedToActivity(binding: ActivityPluginBinding) {
     if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.Q) {
-      binding.activity.registerActivityLifecycleCallbacks(object :Application.ActivityLifecycleCallbacks{
-        override fun onActivityCreated(activity: Activity, savedInstanceState: Bundle?) {
-          Log.d("NATIVE_PLUGIN", "onActivityCreated")
+      binding.activity.application.registerActivityLifecycleCallbacks(
+        object : Application.ActivityLifecycleCallbacks {
+          override fun onActivityCreated(activity: Activity, savedInstanceState: Bundle?) {
+            Log.d("NATIVE_PLUGIN", "onActivityCreated")
+          }
+
+          override fun onActivityStarted(activity: Activity) {
+            Log.d("NATIVE_PLUGIN", "onActivityStarted")
+            val serviceIntent = Intent(context, AudioService::class.java)
+            if (!context.isServiceRunning(AudioService::class.java)) {
+              context.startService(serviceIntent)
+            }
+            serviceConnection?.let {
+              context.bindService(serviceIntent, it, Context.BIND_AUTO_CREATE)
+            }
+          }
+
+          override fun onActivityResumed(activity: Activity) {
+            Log.d("NATIVE_PLUGIN", "onActivityResumed")
+          }
+
+          override fun onActivityPaused(activity: Activity) {
+            Log.d("NATIVE_PLUGIN", "onActivityPaused")
+          }
+
+          override fun onActivityStopped(activity: Activity) {
+            Log.d("NATIVE_PLUGIN", "onActivityStopped")
+          }
+
+          override fun onActivitySaveInstanceState(activity: Activity, outState: Bundle) {}
+
+          override fun onActivityDestroyed(activity: Activity) {
+            Log.d("NATIVE_PLUGIN", "onActivityDestroyed")
+            audioService?.stop()
+          }
         }
-
-        override fun onActivityStarted(activity: Activity) {
-          Log.d("NATIVE_PLUGIN", "onActivityStarted")
-
-          val serviceIntent = Intent(context, AudioService::class.java)
-          if (!context.isServiceRunning(AudioService::class.java)) context.startService(serviceIntent)
-          serviceConnection?.let { context.bindService(serviceIntent, it, Context.BIND_AUTO_CREATE) }
-
-        }
-
-        override fun onActivityResumed(activity: Activity) {
-          Log.d("NATIVE_PLUGIN", "onActivityResumed")
-        }
-
-        override fun onActivityPaused(activity: Activity) {
-          Log.d("NATIVE_PLUGIN", "onActivityPaused")
-        }
-
-        override fun onActivityStopped(activity: Activity) {
-          Log.d("NATIVE_PLUGIN", "onActivityStopped")
-        }
-
-        override fun onActivitySaveInstanceState(activity: Activity, outState: Bundle) {
-        
-        }
-
-        override fun onActivityDestroyed(activity: Activity) {
-          Log.d("NATIVE_PLUGIN", "onActivityDestroyed")
-          audioService?.stop()
-        }
-
-      })
+      )
     }
   }
 
   private fun withService(withService: (AudioService) -> Unit) {
     if (audioService == null) {
-
-      // Audio service not available yes, bind and setup
       serviceConnection = object : ServiceConnection {
         override fun onServiceConnected(name: ComponentName?, binder: IBinder?) {
           val service = (binder as AudioService.AudioServiceBinder).getService()
           bindAudioServiceWithChannel(service)
-          withService(service)
-
           audioService = service
+          withService(service)
         }
 
         override fun onServiceDisconnected(name: ComponentName?) {
@@ -202,34 +215,43 @@ class FlutterPluginAudioPlugin: FlutterPlugin, MethodCallHandler, ActivityAware 
       }
 
       val serviceIntent = Intent(context, AudioService::class.java)
-      if (!context.isServiceRunning(AudioService::class.java)) context.startService(serviceIntent)
-      context.bindService(serviceIntent, serviceConnection as ServiceConnection, Context.BIND_AUTO_CREATE)
-
-      // Return and wait for service to be connected
+      if (!context.isServiceRunning(AudioService::class.java)) {
+        context.startService(serviceIntent)
+      }
+      context.bindService(
+        serviceIntent,
+        serviceConnection as ServiceConnection,
+        Context.BIND_AUTO_CREATE
+      )
       return
     }
 
-    // Call lambda with service
     withService(audioService!!)
   }
 
-
-  private fun <T> withArgument(methodCall: MethodCall, argumentKey: String, withArgument: (T) -> Unit) {
+  private fun <T> withArgument(
+    methodCall: MethodCall,
+    argumentKey: String,
+    withArgument: (T) -> Unit
+  ) {
     val argument = methodCall.argument<T>(argumentKey)
-            ?: throw IllegalArgumentException("Argument $argumentKey is required when calling the ${methodCall.method} method.")
-
+      ?: throw IllegalArgumentException(
+        "Argument $argumentKey is required when calling ${methodCall.method}"
+      )
     withArgument(argument)
   }
 
   private fun bindAudioServiceWithChannel(service: AudioService) {
     service.apply {
-      // Notify flutter with audio updates
       onLoaded = { totalDurationInMillis, startedAutomatically ->
         try {
-          channel.invokeMethod(FLUTTER_METHOD_ON_LOADED, mapOf(
-                  FLUTTER_METHOD_ON_LOADED_ARG_TOTAL_DURATION_IN_MILLIS to totalDurationInMillis,
-                  FLUTTER_METHOD_ON_LOADED_ARG_STARTED_AUTOMATICALLY to startedAutomatically
-          ))
+          channel.invokeMethod(
+            FLUTTER_METHOD_ON_LOADED,
+            mapOf(
+              FLUTTER_METHOD_ON_LOADED_ARG_TOTAL_DURATION_IN_MILLIS to totalDurationInMillis,
+              FLUTTER_METHOD_ON_LOADED_ARG_STARTED_AUTOMATICALLY to startedAutomatically
+            )
+          )
         } catch (e: Exception) {
           Log.e(this::class.java.simpleName, e.message, e)
         }
@@ -275,7 +297,7 @@ class FlutterPluginAudioPlugin: FlutterPlugin, MethodCallHandler, ActivityAware 
         }
       }
 
-      onNext ={
+      onNext = {
         try {
           channel.invokeMethod(FLUTTER_METHOD_ON_NEXT, null)
         } catch (e: Exception) {
@@ -290,7 +312,6 @@ class FlutterPluginAudioPlugin: FlutterPlugin, MethodCallHandler, ActivityAware 
           Log.e(this::class.java.simpleName, e.message, e)
         }
       }
-
     }
   }
 
@@ -299,13 +320,9 @@ class FlutterPluginAudioPlugin: FlutterPlugin, MethodCallHandler, ActivityAware 
     context.stopService(Intent(context, AudioService::class.java))
   }
 
-  override fun onDetachedFromActivityForConfigChanges() {
+  override fun onDetachedFromActivityForConfigChanges() {}
 
-  }
-
-  override fun onReattachedToActivityForConfigChanges(binding: ActivityPluginBinding) {
-
-  }
+  override fun onReattachedToActivityForConfigChanges(binding: ActivityPluginBinding) {}
 
   override fun onDetachedFromActivity() {
     Log.d("NATIVE_PLUGIN", "Detached from Activity")
